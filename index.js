6cac0d6b71dd766964a222da0748df70613107e4
Henry Meehan <henryp.meehan@gmail.com>
2019-10-12 07:00:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 var a = '50';
 var b = '8'
 function add(a, b) {
-add()
+
 }
 
 function subtract(a, b) {
-subtract()
+
 }
 
 function multiply(a, b) {
-multiply()
+
 }
 
 function divide(a, b){
-divide()
+
 }
